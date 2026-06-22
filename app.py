60803e0b5114615b28bd35eaa00d1540322ab317
bricegninafon79-ux <bricegninafon79@gmail.com>
2026-06-22 21:09:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1137,3 +1137,4 @@ elif st.session_state.page == "settings":
     if st.button("✅ Enregistrer", type="primary"):
         st.session_state.user_name = new_name
         st.success("Paramètres enregistrés !")
+        
